9e4c3f81d981efbeff2cd8e61553bcb85891c9a1
Salman Ansari <imsalman315@gmail.com>
2023-02-23 13:11:14 +0530

    initial commit

---
+++ src/store/auth-context.js
@@ -0,0 +1,42 @@
+import { createContext, useEffect, useState } from 'react'
+
+const AuthContext = createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {}
+})
+
+export const AuthContextProvider = props => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn')
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true)
+    }
+  }, [])
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1')
+    setIsLoggedIn(true)
+  }
+
+  const logoutHandler = () => {
+    localStorage.setItem('isLoggedIn', '0')
+    setIsLoggedIn(false)
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContext
